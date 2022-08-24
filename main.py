aa2147c99eb9a659cd950f02d430e5492571cdec
Enes Murat YILDIRIM <enesmy@hotmail.com>
2022-08-25 00:49:44 +0200

    called methods

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from linkedinFastApply import LinkedInEasyApply
+import time
+
 
 searchword = '''C#'''
 location = '''Frankfurt'''
+
+li = LinkedInEasyApply()
+li.setUp()
+li.loadBrowser('https://www.linkedin.com/jobs')
+time.sleep(1)
+li.AcceptEuropenCookies()
+time.sleep(1)
+li.search(searchword, location)
+time.sleep(199)
