e7dcd8260664fbfab6927f6058036a89a0ea2397
Enes Murat YILDIRIM <enesmy@hotmail.com>
2022-08-30 14:08:00 +0200

    user-data-dir chrome option

--- linkedinFastApply.py
+++ linkedinFastApply.py
@@ -1,34 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 import chromedriver_autoinstaller
 import time
 
 
 class LinkedInEasyApply():
-    # xpath list
+
+    # xpath list begin
     XPATH_keywordbox = '//*[@id="JOBS"]/section[1]/input'
     XPATH_locationbox = '//*[@id="JOBS"]/section[2]/input'
     XPATH_locationboxclearbutton = '//*[@id="JOBS"]/section[2]/button'
     XPATH_europaacceptterms = '//*[@id="artdeco-global-alert-container"]/div/section/div/div[2]/button[1]'
     XPATH_searchbutton = '//*[@id="main-content"]/section[1]/div/section/div[2]/button[2]'
+    # xpath list end
+
+    # Do not use this path that is extracted from "chrome://version/"
+    exec_path_chrome = '''C://Users//enesm//AppData//Local//Google//Chrome//User Data'''
 
     def setUp(self):
         chromedriver_autoinstaller.install()
-        self.driver = webdriver.Chrome()
+        self.ch_options = webdriver.ChromeOptions()  # Chrome Options
+        # Extract this path from "chrome://version/"
+        self.ch_options.add_argument(
+            'user-data-dir='+self.exec_path_chrome)
+
+        self.driver = webdriver.Chrome(
+            chrome_options=self.ch_options)
 
     # helper begin
     def WriteElement(self, by, search, value):
-        box = self.driver.find_element(by, search)
-        if (box is None):
-            pass
-        box.send_keys(value)
+        box = self.driver.find_elements(by, search)
+        if (box.__len__() > 0):
+            box[0].send_keys(value)
+            return True
+        return False
 
     def ClickElement(self, by, search):
-        box = self.driver.find_element(by, search)
-        if (box is None):
-            pass
-        box.click()
+        command = self.driver.find_elements(by, search)
+        if (command.__len__() > 0):
+            command[0].click()
+            return True
+        return False
+
     # helper end
 
     def loadBrowser(self, url):
@@ -61,3 +77,12 @@ class LinkedInEasyApply():
         # send form
         self.ClickElement(
             By.XPATH, self.XPATH_searchbutton)
+
+    def nextWorkPosition(self):
+        pass
+
+    def nextPage(self):
+        pass
+
+    def ApplyIfPossible(self):
+        pass
